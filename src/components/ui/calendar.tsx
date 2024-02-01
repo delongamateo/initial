d25406d2c75334f029ae455ce88c345b2643d589
Mateo Delonga <74844680+delongamateo@users.noreply.github.com>
2024-02-01 22:31:46 +0100

    add ui

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -6,7 +6,7 @@ import { DayPicker } from "react-day-picker"
 
 import { cn } from "~/lib/utils"
 import { buttonVariants } from "~/components/ui/button"
-// tslint:disable-next-line
+
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
 function Calendar({
--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -22,7 +22,7 @@ const Command = React.forwardRef<
   />
 ))
 Command.displayName = CommandPrimitive.displayName
-// tslint:disable-next-line
+
 interface CommandDialogProps extends DialogProps {}
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
