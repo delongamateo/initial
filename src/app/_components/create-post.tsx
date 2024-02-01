6ff4f4d0672602113f7f2db8050852c365785781
Mateo Delonga <74844680+delongamateo@users.noreply.github.com>
2024-02-01 20:16:04 +0100

    add openai

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -8,6 +9,36 @@ import { api } from "~/trpc/react";
 export function CreatePost() {
   const router = useRouter();
   const [name, setName] = useState("");
+
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      let response;
+      try {
+        response = await fetch("/api/message", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            message,
+          }),
+        });
+      } catch (err) {
+        console.error(err);
+        throw new Error("Failed to send message");
+      }
+
+      if (!response.ok) {
+        console.error("Status code:", response.status);
+        const responseBody = await response.text();
+        console.error("Response body:", responseBody);
+        throw new Error("Failed to send message");
+      }
+
+      const data = await response.json();
+      return data;
+    },
+  });
 
   const createPost = api.post.create.useMutation({
     onSuccess: () => {
@@ -20,7 +51,7 @@ export function CreatePost() {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
+        sendMessage({ message: name});
       }}
       className="flex flex-col gap-2"
     >
---
+++ src/app/api/message/route.ts
@@ -0,0 +1,36 @@
+import { openai } from "~/utils/openai";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const response = await openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      stream: true,
+      messages: [
+        {
+          role: "system",
+          content:
+            "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+        },
+        {
+          role: "user",
+          content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+            
+      \n----------------\n
+
+      
+      
+      USER INPUT:  ${body.message} \n`,
+        },
+      ],
+    })
+    .catch((err) => {
+      return err;
+    });
+
+  return NextResponse.json({ response });
+
+  return response;
+};
